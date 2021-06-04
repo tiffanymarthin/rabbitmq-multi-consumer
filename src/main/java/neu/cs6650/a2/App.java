aabfed44821bee29cd38979eddfef9823e8ac578
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-06-04 14:48:36 -0700

    Initial commit for the rabbitmq consumer

---
+++ src/main/java/neu/cs6650/a2/App.java
@@ -0,0 +1,13 @@
+package neu.cs6650.a2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
