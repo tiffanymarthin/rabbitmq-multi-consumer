bbcd7712aa0e67bd113d2632cf33ce29ce0857c1
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-06-06 11:06:01 -0700

    Incorporate the static method to update hash map in multithreaded consumer

--- src/main/java/neu/cs6650/a2/MultithreadedConsumer.java
+++ src/main/java/neu/cs6650/a2/MultithreadedConsumer.java
@@ -1,3 +1,8 @@
+/* TODO:
+  - How to print the concurrent hash map after it's completed (race condition)
+  - Implement the pooling
+* */
+
 package neu.cs6650.a2;
 
 import com.rabbitmq.client.Channel;
@@ -6,6 +11,8 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeoutException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -15,6 +22,8 @@ public class MultithreadedConsumer {
   private final static String QUEUE_NAME = "wordCountQueue";
   private final static Integer MAX_MESSAGE_PER_RECEIVER = 1;
   private final static Integer MAX_THREADS = 1;
+
+  private static ConcurrentHashMap<String, Integer> wordMap = new ConcurrentHashMap<>();
 
   public static void main(String[] args) throws IOException, TimeoutException {
     ConnectionFactory factory = new ConnectionFactory();
@@ -29,11 +38,16 @@ public class MultithreadedConsumer {
           channel.queueDeclare(QUEUE_NAME, true, false, false, null);
           // max one message per receiver
           channel.basicQos(MAX_MESSAGE_PER_RECEIVER);
-          logger.info(" [*] Thread waiting for messages. To exit press CTRL+C");
+          System.out.println(" [*] Thread waiting for messages. To exit press CTRL+C");
 
           final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+//            System.out.println(message);
             channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+            System.out.println(WordCount.updateMap(message, wordMap));
+            for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+              System.out.println(entry.getKey() + " val: " + entry.getValue());
+            }
             logger.info("Callback thread ID = " + Thread.currentThread().getId() + " Received: " + message);
           };
           channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
@@ -54,6 +68,10 @@ public class MultithreadedConsumer {
       thread.start();
     }
 
+//    System.out.println(wordMap.size());
+//    for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+//      System.out.println(entry.getKey() + " val: " + entry.getValue());
+//    }
   }
 
 }
