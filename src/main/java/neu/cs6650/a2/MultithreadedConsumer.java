c122be5a6a06ed30c6a839ece3f8da2295563d4a
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-06-07 23:54:13 -0700

    removed printing out map, added todos

--- src/main/java/neu/cs6650/a2/MultithreadedConsumer.java
+++ src/main/java/neu/cs6650/a2/MultithreadedConsumer.java
@@ -1,6 +1,7 @@
 /* TODO:
   - How to print the concurrent hash map after it's completed (race condition)
   - Implement the pooling
+  - Does executor service need to be shut down?
 * */
 
 package neu.cs6650.a2;
@@ -15,7 +16,6 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,7 +24,7 @@ public class MultithreadedConsumer {
   private static final Logger logger = LogManager.getLogger(MultithreadedConsumer.class.getName());
   private final static String QUEUE_NAME = "wordCountQueue";
   private final static Integer MAX_MESSAGE_PER_RECEIVER = 1;
-  private final static Integer MAX_THREADS = 1;
+  private final static Integer MAX_THREADS = 128;
 
   private static ConcurrentHashMap<String, Integer> wordMap = new ConcurrentHashMap<>();
 
@@ -33,30 +33,27 @@ public class MultithreadedConsumer {
     factory.setHost("localhost");
     final Connection connection = factory.newConnection();
 
-    Runnable runnable = new Runnable() {
-      @Override
-      public void run() {
-        try {
-          final Channel channel = connection.createChannel();
-          channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-          // max one message per receiver
-          channel.basicQos(MAX_MESSAGE_PER_RECEIVER);
-          System.out.println(" [*] Thread waiting for messages. To exit press CTRL+C");
+    Runnable runnable = () -> {
+      try {
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+        // max one message per receiver
+        channel.basicQos(MAX_MESSAGE_PER_RECEIVER);
+        System.out.println(" [*] Thread waiting for messages. To exit press CTRL+C");
 
-          final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+        final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+          String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
 //            System.out.println(message);
-            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-            System.out.println(WordCount.updateMap(message, wordMap));
-            for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-              System.out.println(entry.getKey() + " val: " + entry.getValue());
-            }
-            logger.info("Callback thread ID = " + Thread.currentThread().getId() + " Received: " + message);
-          };
-          channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
-        } catch (IOException e) {
-          logger.info(e.getMessage());
-        }
+          channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+//            System.out.println(WordCount.updateMap(message, wordMap));
+          for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+            System.out.println(entry.getKey() + " val: " + entry.getValue());
+          }
+          logger.info("Callback thread ID = " + Thread.currentThread().getId() + " Received: " + message);
+        };
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+      } catch (IOException e) {
+        logger.info(e.getMessage());
       }
     };
 
