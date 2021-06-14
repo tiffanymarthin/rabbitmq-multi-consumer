24ccbe9fd1daba73b7f07c77b260dfc67ffb127b
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-06-14 12:19:26 -0700

    Update MT Consumer class documentation

--- src/main/java/neu/cs6650/a2/MultithreadedConsumer.java
+++ src/main/java/neu/cs6650/a2/MultithreadedConsumer.java
@@ -1,9 +1,3 @@
-/* TODO:
-  - How to print the concurrent hash map after it's completed (race condition)
-  - Implement the pooling (? is this in servlet or here)
-  - Does executor service need to be shut down?
-* */
-
 package neu.cs6650.a2;
 
 import com.rabbitmq.client.Channel;
@@ -11,37 +5,46 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.TimeoutException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ * This class represents a simple multi-threaded consumer
+ * It will consume messages from RabbitMQ and update the wordcount hashmap
+ */
 public class MultithreadedConsumer {
+
   private static final Logger logger = LogManager.getLogger(MultithreadedConsumer.class.getName());
   private final static String QUEUE_NAME = "wordCountQueue";
   private final static Integer MAX_MESSAGE_PER_RECEIVER = 1;
-  private static Integer MAX_THREADS = 5;
-  private static String host_name = "34.217.114.96";
+  private static Integer MAX_THREADS = 32;
 
   private static ConcurrentHashMap<String, Integer> wordMap = new ConcurrentHashMap<>();
 
   public static void main(String[] args) throws IOException, TimeoutException {
     // Command line arguments for easier tests
     //TODO validates the inputs
-    if (args.length != 0) {
-      host_name = args[0];
-      MAX_THREADS = Integer.parseInt(args[1]);
+    if (args.length == 1) {
+      MAX_THREADS = Integer.parseInt(args[0]);
+    }
+
+    Properties prop = new Properties();
+    try (InputStream input = Thread.currentThread().getContextClassLoader()
+        .getResourceAsStream("config.properties")) {
+      prop.load(input);
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
 
     ConnectionFactory factory = new ConnectionFactory();
-//    factory.setHost("localhost");
-    // TODO REMOVE THIS OR USE VENV
-    factory.setUsername("test");
-    factory.setPassword("test");
-    factory.setHost(host_name);
+    factory.setUsername(prop.getProperty("rabbit.username"));
+    factory.setPassword(prop.getProperty("rabbit.password"));
+    factory.setHost(prop.getProperty("rabbit.ip"));
     final Connection connection = factory.newConnection();
 
     Runnable runnable = () -> {
@@ -50,51 +53,26 @@ public class MultithreadedConsumer {
         channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         // max one message per receiver
         channel.basicQos(MAX_MESSAGE_PER_RECEIVER);
-        System.out.println(" [*] Thread waiting for messages. To exit press CTRL+C");
 
         final DeliverCallback deliverCallback = (consumerTag, delivery) -> {
           String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
-//            System.out.println(message);
           channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
           WordCount.updateMap(message, wordMap);
-//          for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-//            System.out.println(entry.getKey() + " val: " + entry.getValue());
-//          }
 //          logger.info("Callback thread ID = " + Thread.currentThread().getId() + " Received: " + message);
         };
-        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
+
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> {});
       } catch (IOException e) {
         logger.info(e.getMessage());
       }
     };
 
-    ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
+//    ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
 
     for (int i = 0; i < MAX_THREADS; i++) {
       Thread thread = new Thread(runnable);
       thread.start();
 //      executorService.submit(runnable);
     }
-
-//    try {
-//      int tries = 0;
-//      executorService.shutdown();
-//      while (!executorService.awaitTermination(5, TimeUnit.SECONDS)) {
-//        executorService.shutdownNow();
-//        if (++tries > 3) {
-//          logger.fatal("Unable to shutdown thread executor. Calling System.exit()...");
-//          System.exit(0);
-//        }
-//      }
-//    } catch (InterruptedException e) {
-//      executorService.shutdownNow();
-//      Thread.currentThread().interrupt();
-//    }
-
-//    System.out.println(wordMap.size());
-//    for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-//      System.out.println(entry.getKey() + " val: " + entry.getValue());
-//    }
   }
-
 }
